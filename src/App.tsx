7cf357b16f08c3a92f6d1c93fdf33d001ca83be3
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-05-24 03:59:10 +0600

    end of project

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Task from './components/Task/Task.tsx';
 interface TaskList {
     task: string;
     id: string;
-    completed:boolean
+    completed: boolean;
 }
 
 const App = () => {
@@ -14,11 +14,10 @@ const App = () => {
     const [editTaskId, setEditTaskId] = useState<string>('');
     const [editTask, setEditTask] = useState<boolean>(false);
     const [taskList, setTaskList] = useState<TaskList[]>([
-        {task: 'create project', id: '1task' , completed:false},
-        {task: 'create components', id: '2task', completed:true},
-        {task: 'create function', id: '3task', completed:false}
+        {task: 'create project', id: '1task', completed: false},
+        {task: 'create components', id: '2task', completed: true},
+        {task: 'create function', id: '3task', completed: false}
     ]);
-
 
     const onHandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newTask = event.target.value;
@@ -31,7 +30,7 @@ const App = () => {
                 const newTask = {
                     task: currentTask,
                     id: String(taskCopy.length + 1) + 'task',
-                    completed:false
+                    completed: false
                 };
                 taskCopy.push(newTask);
                 return taskCopy;
@@ -39,7 +38,6 @@ const App = () => {
             setCurrentTask('');
         }
     };
-
     const onHandleDelete = (id: string) => {
         setTaskList(prevTaskList => {
             return prevTaskList.filter((task) => task.id !== id);
@@ -54,7 +52,7 @@ const App = () => {
     const onEdit = () => {
         if (currentTask !== '') {
             setTaskList(prevTaskList => {
-                return prevTaskList.map((el) => {
+                return prevTaskList.map((el): TaskList => {
                     if (el.id === editTaskId) {
                         return {...el, task: currentTask};
                     }
@@ -64,17 +62,17 @@ const App = () => {
             setEditTaskId('');
             setEditTask(!editTask);
             setCurrentTask('');
-        }else {
-            alert('You cant do empty task, if you wanna delete task: click cancel and click delete on task to delete')
+        } else {
+            alert('You cant do empty task, if you wanna delete task: click cancel and click delete on task to delete');
         }
 
     };
-    const onCancelHandler = ()=>{
+    const onCancelHandler = () => {
         setEditTaskId('');
         setEditTask(!editTask);
         setCurrentTask('');
-    }
-    const onCompleteTask = (id:string)=>{
+    };
+    const onCompleteTask = (id: string) => {
         setTaskList(prevTaskList => {
             return prevTaskList.map((el) => {
                 if (el.id === id) {
@@ -83,10 +81,11 @@ const App = () => {
                 return el;
             });
         });
-    }
+    };
 
     return (
         <>
+            <h3> To Do List App</h3>
             <AddTaskForm value={currentTask}
                          onBtnClick={onHandleClick}
                          onInputChange={onHandleChange}
@@ -94,12 +93,12 @@ const App = () => {
                          onEdit={onEdit}
                          onCancel={onCancelHandler}
             />
-                {editTask?<h4>Edit your task(task id: {editTaskId})</h4> :
-                    taskList.map((el) => {
+            {editTask ? <h4>Edit your task(task id: {editTaskId})</h4> :
+                taskList.map((el) => {
                     return <Task task={el.task}
                                  id={el.id}
                                  completed={el.completed}
-                                 onComplete={()=>onCompleteTask(el.id)}
+                                 onComplete={() => onCompleteTask(el.id)}
                                  onDeleteClick={() => onHandleDelete(el.id)}
                                  onEditTask={() => onEditTask(el.id)}
                                  key={el.id}
